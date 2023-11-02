c79f427c1e0a328e671642014f5fcaaab8a522be
NguyenLeCongQuy <nguyenlecongquy@gmail.com>
2023-11-02 13:33:35 +0700

    UI1 - Start Screen

--- App.js
+++ App.js
@@ -1,13 +1,9 @@
 import { StyleSheet, Text, View } from 'react-native';
 
-import ExampleScreen from './src/screens/ExampleScreen';
+import StartScreen from './src/screens/Start';
 
 export default function App() {
   return (
-    // tạo 1 screen mới trong src/screen
-    // import vào đây
-    // có thể tạo components thì để trong src/components
-    <ExampleScreen/>
-    // đây là màn hình ví dụ nha, tạo branch mới thì xóa tag này đi, chỉ để tag bạn làm vào đây
+    <StartScreen/>
   );
 }
