17ecf56a66e5d0cb15e47b8c3edf9ee51bcfe0ea
nguyeh8 <nguyentran.07052002ak@gmail.com>
2023-11-02 21:57:05 +0700

    nguyen/UI3

--- App.js
+++ App.js
@@ -1,13 +1,11 @@
 import { StyleSheet, Text, View } from 'react-native';
 
 import ExampleScreen from './src/screens/ExampleScreen';
+import UI3Screen from './src/screens/UI3Screen';
 
 export default function App() {
   return (
-    // tạo 1 screen mới trong src/screen
-    // import vào đây
-    // có thể tạo components thì để trong src/components
-    <ExampleScreen/>
-    // đây là màn hình ví dụ nha, tạo branch mới thì xóa tag này đi, chỉ để tag bạn làm vào đây
+ 
+    <UI3Screen />
   );
 }
