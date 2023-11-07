e3b3b2e9b1c0760849d6e8d421c604af65c0e5f8
Trần Nguyễn Quy <kekkei2002@gmail.com>
2023-11-08 00:34:14 +0700

    Update react navigation

--- App.js
+++ App.js
@@ -1,6 +1,10 @@
 import ExampleScreen from "./src/screens/ExampleScreen";
 import ErrorBoundary from "./src/containers/ErrorBoundary";
 import { NativeBaseProvider } from "native-base";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
@@ -9,7 +13,13 @@ export default function App() {
     // có thể tạo components thì để trong src/components
     <ErrorBoundary>
       <NativeBaseProvider>
-        <ExampleScreen />
+        <NavigationContainer>
+          <Stack.Navigator>
+            <Stack.Screen name="Home" component={ExampleScreen} />
+            <Stack.Screen name="Login" component={ExampleScreen} />
+            <Stack.Screen name="Signup" component={ExampleScreen} />
+          </Stack.Navigator>
+        </NavigationContainer>
       </NativeBaseProvider>
     </ErrorBoundary>
     // đây là màn hình ví dụ nha, tạo branch mới thì xóa tag này đi, chỉ để tag bạn làm vào đây
