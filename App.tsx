e232f08a3370de9884c50a365a5cf7f72458cf98
Trần Nguyễn Quy <kekkei2002@gmail.com>
2023-11-08 14:36:56 +0700

    Setup Libraries & Typescript (#2)
    * Update native base package
    
    * Update react navigation
    
    * Setup Typescript

---
+++ App.tsx
@@ -0,0 +1,27 @@
+import { NativeBaseProvider } from "native-base";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { NavigationContainer } from "@react-navigation/native";
+import ErrorBoundary from "./src/containers/ErrorBoundary";
+import ExampleScreen from "./src/screens/ExampleScreen";
+
+const Stack = createNativeStackNavigator();
+
+export default function App() {
+  return (
+    // tạo 1 screen mới trong src/screen
+    // import vào đây
+    // có thể tạo components thì để trong src/components
+    <ErrorBoundary>
+      <NativeBaseProvider>
+        <NavigationContainer>
+          <Stack.Navigator>
+            <Stack.Screen name="Home" component={ExampleScreen} />
+            <Stack.Screen name="Login" component={ExampleScreen} />
+            <Stack.Screen name="Signup" component={ExampleScreen} />
+          </Stack.Navigator>
+        </NavigationContainer>
+      </NativeBaseProvider>
+    </ErrorBoundary>
+    // đây là màn hình ví dụ nha, tạo branch mới thì xóa tag này đi, chỉ để tag bạn làm vào đây
+  );
+}
