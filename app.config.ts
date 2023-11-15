d4a3fe8f679472fab30289003c91b2236e345cfd
NguyenLeCongQuy <nguyenlecongquy@gmail.com>
2023-11-16 00:10:48 +0700

    config CI/CD 2-times

--- app.config.ts
+++ app.config.ts
@@ -1,8 +1,8 @@
 import { ExpoConfig, ConfigContext } from 'expo/config';
 
 export default ({ config }: ConfigContext): ExpoConfig => ({
-  name: 'test-app-build',
-  slug: 'test-app-build',
+  name: 'SleepBetter',
+  slug: 'SleepBetter',
   version: config.version,
   icon: config.icon,
   extra: config.extra,
